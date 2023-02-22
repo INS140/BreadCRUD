0704a41996e26a20ac7e680226d1f64b97d5bb57
INS140 <IsaacNSaettel@gmail.com>
2023-02-21 20:36:24 -0800

    first helper method

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -33,21 +33,25 @@ breads.get('/data/seed', (req, res) => {
           name: 'Rye',
           hasGluten: true,
           image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Rachel'
         },
         {
           name: 'French',
           hasGluten: true,
           image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+          baker: 'Rachel'
         },
         {
           name: 'Gluten Free',
           hasGluten: false,
           image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Rachel'
         },
         {
           name: 'Pumpernickel',
           hasGluten: true,
           image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Rachel'
         }
     ]).then(() => res.redirect('/breads'))
 })
@@ -55,7 +59,7 @@ breads.get('/data/seed', (req, res) => {
 // SHOW
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
-        .then(data => {res.render('show', { bread: data })})
+        .then(bread =>  res.render('show', { bread }) )
         .catch(err => {
             console.log(err)
             res.status(404).render('error404')
