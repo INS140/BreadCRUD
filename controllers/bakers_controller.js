d8d0ebb8f868a91bea527558b7e3649f1bfe73b7
INS140 <IsaacNSaettel@gmail.com>
2023-02-26 11:25:19 -0800

    cleanup and refactor

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -3,36 +3,34 @@ const baker = express.Router()
 const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
-baker.get('/', (req, res) => {
-    Baker.find()
-        .populate('breads')
-        .then(bakers => {
-            res.send(bakers)
-        })
+// Object Array Reference
+baker.get('/', async (req, res) => {
+    const bakers = Baker.find().populate('breads')
+    res.send(bakers)
 })
 
-baker.get('/data/seed', (req, res) => {
-    Baker.insertMany(bakerSeedData)
-        .then(res.redirect('/breads'))
+// SEED DATA
+baker.get('/data/seed', async (req, res) => {
+    await Baker.insertMany(bakerSeedData)
+    res.redirect('/breads')
 })
 
-baker.get('/:id', (req, res) => {
-    Baker.findById(req.params.id)
-        .populate('breads')
-        .then(baker => {
-            res.render('bakerShow', { baker: baker })
-        })
+// SHOW
+baker.get('/:id', async (req, res) => {
+    const baker = await Baker.findById(req.params.id).populate('breads')
+    res.render('bakerShow', { baker: baker })
 })
 
-baker.delete('/:id', (req, res) => {
-    Baker.findByIdAndDelete(req.params.id)
-        .then(baker => {
-            res.status(303).redirect('/breads')
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(404).render('error404')
-        })
+// DELETE
+baker.delete('/:id', async (req, res) => {
+    try {
+        await Baker.findByIdAndDelete(req.params.id)
+        res.status(303).redirect('/breads')
+    }
+    catch(err) {
+        console.log(err)
+        res.status(404).render('error404')
+    }
 })
 
-module.exports = baker                    
+module.exports = baker
--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,106 +3,94 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 const Baker = require('../models/baker.js')
 
-breads.get('/', (req, res) => {
-    Baker.find()
-        .then(bakers => {
-            Bread.find().then(breads => {
-                res.render('index', {
-                    breads: breads,
-                    bakers: bakers,
-                    title: 'Index Page'
-                })
-            })
-        })
-    
+breads.get('/', async (req, res) => {
+    const bakers = await Baker.find()
+    const breads = await Bread.find()
+    res.render('index', {
+        breads: breads,
+        bakers: bakers,
+        title: 'Index Page'
+    })
 })
 
 // CREATE
-breads.post('/', (req, res) => {
+breads.post('/', async (req, res) => {
     if (!req.body.image) {
         req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
     }
     req.body.hasGluten = req.body.hasGluten === 'on'
-    Bread.create(req.body)
-        .then(() => res.redirect('/breads'))
+    await Bread.create(req.body)
+    res.redirect('/breads')
 })
 
 // NEW
-breads.get('/new', (req, res) => {
-    Baker.find().then(foundBakers => {
-        res.render('new', { bakers: foundBakers })
-    })
+breads.get('/new', async (req, res) => {
+    const foundBakers = await Baker.find()
+    res.render('new', { bakers: foundBakers })
 })
 
 // SEED DATA
-breads.get('/data/seed', (req, res) => {
-    Baker.findOne({name: 'Rachel'}).then(baker => {
-        Bread.insertMany([
-            {
-              name: 'Rye',
-              hasGluten: true,
-              image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-              baker: baker
-            },
-            {
-              name: 'French',
-              hasGluten: true,
-              image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-              baker: baker
-            },
-            {
-              name: 'Gluten Free',
-              hasGluten: false,
-              image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-              baker: baker
-            },
-            {
-              name: 'Pumpernickel',
-              hasGluten: true,
-              image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-              baker: baker
-            }
-        ]).then(() => res.redirect('/breads'))
-    })
+breads.get('/data/seed', async (req, res) => {
+    const baker = await Baker.findOne({name: 'Rachel'})
+    await Bread.insertMany([
+        {
+            name: 'Rye',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            baker: baker
+        },
+        {
+            name: 'French',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            baker: baker
+        },
+        {
+            name: 'Gluten Free',
+            hasGluten: false,
+            image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+            baker: baker
+        },
+        {
+            name: 'Pumpernickel',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+            baker: baker
+        }
+    ])
+    res.redirect('/breads')
 })
 
 // SHOW
-breads.get('/:id', (req, res) => {
-    Bread.findById(req.params.id)
-        .populate('baker')
-        .then(bread =>  res.render('show', { bread }) )
-        .catch(err => {
-            console.log(err)
-            res.status(404).render('error404')
-        })
+breads.get('/:id', async (req, res) => {
+    try {
+        const bread = await Bread.findById(req.params.id).populate('baker')
+        res.render('show', { bread })
+    }
+    catch(err) {
+        console.log(err)
+        res.status(404).render('error404')
+    }
 })
 
 // DELETE
-breads.delete('/:id', (req, res) => {
-    Bread.findByIdAndDelete(req.params.id)
-        .then(() => {
-            res.status(303).redirect('/breads')
-        })
+breads.delete('/:id', async (req, res) => {
+    await Bread.findByIdAndDelete(req.params.id)
+    res.status(303).redirect('/breads')
 })
 
 // UPDATE
-breads.put('/:id', (req, res) => {
+breads.put('/:id', async (req, res) => {
     req.body.hasGluten = req.body.hasGluten === 'on'
-    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then(() => {  
-            res.redirect(`/breads/${req.params.id}`)
-        })
+    await Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.redirect(`/breads/${req.params.id}`)
 })
 
 // EDIT
-breads.get('/:id/edit', (req, res) => {
-    Baker.find()
-        .then(bakers => {
-            Bread.findById(req.params.id)
-                .then(bread => {
-                    res.render('edit', { bread: bread, bakers: bakers })
-                })
-        })
+breads.get('/:id/edit', async (req, res) => {
+    const bakers = await Baker.find()
+    const bread = await Bread.findById(req.params.id)
+    res.render('edit', { bread: bread, bakers: bakers })
 })
 
 module.exports = breads
